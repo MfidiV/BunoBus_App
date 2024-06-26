a2ea89bbf74aad550a0a720d0ff4f6abae250600
Bunono <mfidiv@gmail.com>
2024-06-26 09:26:05 +0200

    removed the backend app

--- Backend/BusApp/urls.py
+++
@@ -1,9 +0,0 @@
-# users/urls.py
-
-from django.urls import path
-from .views import UserCreateView
-
-urlpatterns = [
-    path('register/', UserCreateView.as_view(), name='user-register'),
-    # Add more endpoints as needed
-]
