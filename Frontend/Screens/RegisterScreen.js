012a57566b77100c23572759a53f4f4f9a1011c5
Bunono <mfidiv@gmail.com>
2024-06-27 16:51:21 +0200

    feat: Make registration form scrollable and more compact
    - Wrapped form content inside a ScrollView to ensure it is scrollable.
    - Adjusted padding and margin to reduce form size and make it more compact.
    - Ensured form content remains centered and accessible.
    - Added validation for name, surname, and email inputs.
    - Enhanced user experience with better error handling and feedback.

--- Frontend/Screens/RegisterScreen.js
+++ Frontend/Screens/RegisterScreen.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import React, { useState } from 'react';
-import { View, Text, TextInput, Alert, TouchableOpacity, StyleSheet, TouchableWithoutFeedback, KeyboardAvoidingView, Platform } from 'react-native';
+import { View, Text, TextInput, Alert, TouchableOpacity, StyleSheet, TouchableWithoutFeedback, KeyboardAvoidingView, Platform, ScrollView } from 'react-native';
 import { MaterialIcons } from '@expo/vector-icons';
 
 const RegisterScreen = ({ navigation }) => {
@@ -12,11 +12,36 @@ const RegisterScreen = ({ navigation }) => {
   const [showPassword, setShowPassword] = useState(false);
   const [acceptTerms, setAcceptTerms] = useState(false);
 
+  // Regular expressions for validations
+  const nameSurnameRegex = /^[a-zA-Z]{3,}$/;
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
   const handleRegister = async () => {
+    // Check if terms and conditions are accepted
     if (!acceptTerms) {
       Alert.alert('Please accept the terms and conditions');
       return;
     }
+
+    // Validate name
+    if (!nameSurnameRegex.test(name)) {
+      Alert.alert('Name should be at least 3 characters long and contain only letters');
+      return;
+    }
+
+    // Validate surname
+    if (!nameSurnameRegex.test(surname)) {
+      Alert.alert('Surname should be at least 3 characters long and contain only letters');
+      return;
+    }
+
+    // Validate email
+    if (!emailRegex.test(email)) {
+      Alert.alert('Please enter a valid email address');
+      return;
+    }
+
+    // Check if passwords match
     if (password !== confirmPassword) {
       Alert.alert('Passwords do not match');
       return;
@@ -29,7 +54,6 @@ const RegisterScreen = ({ navigation }) => {
       default: 'http://192.168.0.2:8000'
     });
 
-
     try {
       const response = await axios.post(`${baseURL}/api/users/`, {
         name,
@@ -37,6 +61,7 @@ const RegisterScreen = ({ navigation }) => {
         email,
         password,
       });
+
       if (response.status === 201) {
         Alert.alert('User registered successfully');
         navigation.navigate('Login');  // Navigate to Login screen
@@ -54,67 +79,69 @@ const RegisterScreen = ({ navigation }) => {
       style={styles.container}
       behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
     >
-      <View style={styles.inner}>
-        <Text style={styles.title}>Create Account</Text>
-        <TextInput
-          style={styles.input}
-          placeholder="Name"
-          value={name}
-          onChangeText={setName}
-        />
-        <TextInput
-          style={styles.input}
-          placeholder="Surname"
-          value={surname}
-          onChangeText={setSurname}
-        />
-        <TextInput
-          style={styles.input}
-          placeholder="Email"
-          value={email}
-          onChangeText={setEmail}
-          keyboardType="email-address"
-          autoCapitalize="none"
-        />
-        <TextInput
-          style={styles.input}
-          placeholder="Password"
-          secureTextEntry={!showPassword}
-          value={password}
-          onChangeText={setPassword}
-        />
-        <TextInput
-          style={styles.input}
-          placeholder="Confirm Password"
-          secureTextEntry={!showPassword}
-          value={confirmPassword}
-          onChangeText={setConfirmPassword}
-        />
-        <View style={styles.checkboxContainer}>
-          <TouchableWithoutFeedback onPress={() => setShowPassword(!showPassword)}>
-            <View style={styles.checkbox}>
-              {showPassword && <MaterialIcons name="check-box" size={24} color="#6200ea" />}
-              {!showPassword && <MaterialIcons name="check-box-outline-blank" size={24} color="#6200ea" />}
-            </View>
-          </TouchableWithoutFeedback>
-          <Text style={styles.checkboxLabel}>Show Password</Text>
+      <ScrollView contentContainerStyle={styles.scrollContainer}>
+        <View style={styles.inner}>
+          <Text style={styles.title}>Create Account</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Name"
+            value={name}
+            onChangeText={setName}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="Surname"
+            value={surname}
+            onChangeText={setSurname}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="Email"
+            value={email}
+            onChangeText={setEmail}
+            keyboardType="email-address"
+            autoCapitalize="none"
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="Password"
+            secureTextEntry={!showPassword}
+            value={password}
+            onChangeText={setPassword}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="Confirm Password"
+            secureTextEntry={!showPassword}
+            value={confirmPassword}
+            onChangeText={setConfirmPassword}
+          />
+          <View style={styles.checkboxContainer}>
+            <TouchableWithoutFeedback onPress={() => setShowPassword(!showPassword)}>
+              <View style={styles.checkbox}>
+                {showPassword && <MaterialIcons name="check-box" size={20} color="#6200ea" />}
+                {!showPassword && <MaterialIcons name="check-box-outline-blank" size={20} color="#6200ea" />}
+              </View>
+            </TouchableWithoutFeedback>
+            <Text style={styles.checkboxLabel}>Show Password</Text>
+          </View>
+          <View style={styles.checkboxContainer}>
+            <TouchableWithoutFeedback onPress={() => setAcceptTerms(!acceptTerms)}>
+              <View style={styles.checkbox}>
+                {acceptTerms && <MaterialIcons name="check-box" size={20} color="#6200ea" />}
+                {!acceptTerms && <MaterialIcons name="check-box-outline-blank" size={20} color="#6200ea" />}
+              </View>
+            </TouchableWithoutFeedback>
+            <Text style={styles.checkboxLabel}>I accept the Terms and Conditions</Text>
+          </View>
+          <TouchableOpacity style={styles.button} onPress={handleRegister}>
+            <Text style={styles.buttonText}>Register</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.buttonSecondary} onPress={() => navigation.navigate('Login')}>
+            <Text style={styles.buttonTextSecondary}>Back to Login</Text>
+          </TouchableOpacity>
         </View>
-        <View style={styles.checkboxContainer}>
-          <TouchableWithoutFeedback onPress={() => setAcceptTerms(!acceptTerms)}>
-            <View style={styles.checkbox}>
-              {acceptTerms && <MaterialIcons name="check-box" size={24} color="#6200ea" />}
-              {!acceptTerms && <MaterialIcons name="check-box-outline-blank" size={24} color="#6200ea" />}
-            </View>
-          </TouchableWithoutFeedback>
-          <Text style={styles.checkboxLabel}>I accept the Terms and Conditions</Text>
-        </View>
-        <TouchableOpacity style={styles.button} onPress={handleRegister}>
-          <Text style={styles.buttonText}>Register</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.buttonSecondary} onPress={() => navigation.navigate('Login')}>
-          <Text style={styles.buttonTextSecondary}>Back to Login</Text>
-        </TouchableOpacity>
-      </View>
+      </ScrollView>
     </KeyboardAvoidingView>
   );
 };
@@ -124,15 +151,18 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     backgroundColor: '#f7f7f7',
-    padding: 20,
+    padding: 50,
+  },
+  scrollContainer: {
+    flexGrow: 1,
+    justifyContent: 'center',
   },
   inner: {
-    flex: 1,
     justifyContent: 'center',
   },
   title: {
-    fontSize: 28,
-    marginBottom: 20,
+    fontSize: 24,
+    marginBottom: 15,
     textAlign: 'center',
     fontWeight: 'bold',
     color: '#333',
@@ -140,18 +170,18 @@ const styles = StyleSheet.create({
   input: {
     borderBottomWidth: 1,
     borderColor: '#6200ea',
-    padding: 10,
-    marginVertical: 10,
+    padding: 3,
+    marginVertical: 8,
     backgroundColor: '#fff',
     borderRadius: 5,
   },
   checkboxContainer: {
     flexDirection: 'row',
     alignItems: 'center',
-    marginVertical: 10,
+    marginVertical: 8,
   },
   checkbox: {
-    marginRight: 10,
+    marginRight: 8,
   },
   checkboxLabel: {
     fontSize: 14,
@@ -159,27 +189,27 @@ const styles = StyleSheet.create({
   },
   button: {
     backgroundColor: '#6200ea',
-    padding: 15,
+    padding: 12,
     borderRadius: 8,
     alignItems: 'center',
-    marginVertical: 10,
+    marginVertical: 8,
   },
   buttonText: {
     color: '#fff',
-    fontSize: 16,
+    fontSize: 14,
     fontWeight: 'bold',
   },
   buttonSecondary: {
-    padding: 15,
+    padding: 12,
     borderRadius: 8,
     alignItems: 'center',
-    marginVertical: 10,
+    marginVertical: 8,
     borderWidth: 1,
     borderColor: '#6200ea',
   },
   buttonTextSecondary: {
     color: '#6200ea',
-    fontSize: 16,
+    fontSize: 14,
     fontWeight: 'bold',
   },
 });
